7e7ad1c58dffd489b71c04ed2d391dcb45db8bed
Nazlıcan Tokal <nazlicantokal@gmail.com>
2026-01-27 23:06:27 +0300

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+# sehirler = ['kocaeli','istanbul']
+# plakalar = [41,34]
+
+# #key-value
+# print(plakalar[0], sehirler[0])
+# print(plakalar[1],sehirler[1])
+
+# print(plakalar[sehirler.index('istanbul')])
+# print(plakalar[sehirler.index('kocaeli')])
+
+### dictionary  (key:value)
+
+plakalar = {
+    'kocaeli':41,
+    'istanbul':34,
+    }
+
+print(plakalar['kocaeli'])
